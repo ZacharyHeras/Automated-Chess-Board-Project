809543481fbb39af1553bb35b48badb9fa935474
Zachary Heras <zacharyheras@gmail.com>
2021-05-13 21:54:44 -0400

    Added move scraper and chromedriver for use with selenium

---
+++ chess.com-move-scraper.py
@@ -0,0 +1,12 @@
+'''
+Scrapes chess.com for piece positions of current game.
+Prints pieces positions to console as they get updated.
+
+PrinceZ27, May 13th, 2021
+
+'''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys as keys
+
+driver = webdriver.()
+driver.get(www.google.com)
